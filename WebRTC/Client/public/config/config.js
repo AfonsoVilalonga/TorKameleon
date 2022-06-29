7fd8b9bf2e002b535b5d3810772eee765c76794f
AfonsoVi <j.vilalonga@campus.fct.unl.pt>
2022-06-29 21:56:54 +0100

    commit

--- WebRTC/Client/public/config/config.js
+++ WebRTC/Client/public/config/config.js
@@ -1,18 +1,18 @@
 window.webrtc = {
     iceServers: [{
-        urls: [ "stun:eu-turn5.xirsys.com" ]
+        urls: [ "stun:eu-turn4.xirsys.com" ]
      }, {
-        username: "J3y90t6JVJ4HNEOJcV6NqaprxU_vaXpFP-buT48SDdaAR5RKLTumz0eY0oTWMCfWAAAAAGK4nMd0ZXN0ZXdlYnJ0YzEyMw==",
-        credential: "b1ee12d2-f578-11ec-a528-0242ac140004",
+        username: "Es2MF1dNpixy1MuvrIgjIT-5R33ZlXc8sj1AoJQJka8Kk9_guwMDUmFHDlswCxfMAAAAAGK8vFF0ZXN0ZXdlYnJ0Y29pMTIzNDU=",
+        credential: "d8c91006-f7ed-11ec-a2a0-0242ac140004",
         urls: [
-            "turn:eu-turn5.xirsys.com:80?transport=udp",
-            "turn:eu-turn5.xirsys.com:3478?transport=udp",
-            "turn:eu-turn5.xirsys.com:80?transport=tcp",
-            "turn:eu-turn5.xirsys.com:3478?transport=tcp",
-            "turns:eu-turn5.xirsys.com:443?transport=tcp",
-            "turns:eu-turn5.xirsys.com:5349?transport=tcp"
+            "turn:eu-turn4.xirsys.com:80?transport=udp",
+            "turn:eu-turn4.xirsys.com:3478?transport=udp",
+            "turn:eu-turn4.xirsys.com:80?transport=tcp",
+            "turn:eu-turn4.xirsys.com:3478?transport=tcp",
+            "turns:eu-turn4.xirsys.com:443?transport=tcp",
+            "turns:eu-turn4.xirsys.com:5349?transport=tcp"
         ]
-     }], 
+     }],
     encodedInsertableStreams: true
 };
 

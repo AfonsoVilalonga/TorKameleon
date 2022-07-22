f324493b2a3cdda83d236fdea91fb01fdb30ae89
AfonsoVi <j.vilalonga@campus.fct.unl.pt>
2022-07-22 01:13:26 +0100

    commit

---
+++ Client/src/main/java/com/afonsovilalonga/AutomatedClient/Injector.java
@@ -0,0 +1,160 @@
+package com.afonsovilalonga.AutomatedClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+import com.afonsovilalonga.Utils.Config;
+import com.afonsovilalonga.Utils.DTLSOverDatagram;
+import com.afonsovilalonga.Utils.Stats;
+
+public class Injector implements Runnable{
+    
+    private String file;
+    private String protocol;
+    private int num_reqs;
+
+    public static final int TIMEOUT = 50000;
+    public static final int BUF_SIZE = 4096;
+
+    public Injector(String protocol, String file, int num_reqs){
+        this.file = file;
+        this.protocol = protocol;
+        this.num_reqs = num_reqs;
+    }
+
+    @Override
+    public void run() {
+        try {
+            executeCommand();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void executeCommand() throws Exception {
+        Config config = Config.getInstance();
+        String remote_host = config.get_remote_host();
+        int remote_port_secure = config.getRemote_port_secure();
+        int remote_port_unsecure = config.getRemote_port_unsecure();
+
+        switch (protocol.toLowerCase()) {
+            case "tcp":
+                Socket tcp_socket = new Socket(remote_host, remote_port_unsecure);
+                for(int i = 0; i < num_reqs; i++)
+                    do_TCP_TLS(tcp_socket, file);
+                tcp_socket.close();
+                break;
+            case "tls":
+                Socket tls_socket = getSecureSocket(remote_host, remote_port_secure);
+                for(int i = 0; i < num_reqs; i++)
+                    do_TCP_TLS(tls_socket, file);
+                tls_socket.close();
+                break;
+            case "udp":
+                DatagramSocket udp_socket = new DatagramSocket();
+                for(int i = 0; i < num_reqs; i++)
+                    doUDP(file.getBytes(), udp_socket, remote_host, remote_port_unsecure);
+                udp_socket.close();
+                break;
+            case "dtls":
+                DatagramSocket dtls_socket = new DatagramSocket();
+                for(int i = 0; i < num_reqs; i++)
+                    doDTLS(dtls_socket, file.getBytes(), remote_host, remote_port_secure);
+                dtls_socket.close();
+                break;
+        }
+
+    }
+
+    private static Socket getSecureSocket(String host, int port) throws IOException {
+        SSLSocketFactory factory =
+                (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket =
+                (SSLSocket) factory.createSocket(host, port);
+        socket.startHandshake();
+        return socket;
+    }
+
+    private static void do_TCP_TLS(Socket socket, String path) throws IOException {
+        OutputStream out = socket.getOutputStream();
+        InputStream in = socket.getInputStream();
+        Stats stats = new Stats();
+
+        out.write(String.format("GET %s HTTP/1.1", path).getBytes());
+
+        int n = 0;
+        byte[] buffer = new byte[BUF_SIZE];
+        while ((n = in.read(buffer, 0, buffer.length)) != -1) {
+            stats.newRequest(n);
+            System.out.write(buffer, 0, n);
+        }
+        stats.printReport();
+    }
+
+    private static void doUDP(byte[] path, DatagramSocket socket, String host, int port) {
+        try {
+            socket.setSoTimeout(TIMEOUT);
+            InetAddress IPAddress = InetAddress.getByName(host);
+            Stats stats = new Stats();
+            DatagramPacket sendPacket = new DatagramPacket(path, path.length, IPAddress, port);
+            socket.send(sendPacket);
+            //receive
+            byte[] receive = new byte[BUF_SIZE];
+            DatagramPacket receivedPacket = new DatagramPacket(receive, receive.length);
+            try {
+                while (true) {
+                    socket.receive(receivedPacket);
+                    String receivedData = new String(receivedPacket.getData());
+                    if (receivedData.contains("terminate_packet_receive")) {
+                        break;
+                    }
+                    stats.newRequest(receivedPacket.getLength());
+                    System.out.println(receivedData);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            socket.close();
+            stats.printReport();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Unable to do UDP");
+        }
+    }
+
+    private static void doDTLS(DatagramSocket socket, byte[] filePath, String host, int port) {
+        try {
+            socket.setSoTimeout(TIMEOUT);
+            DTLSOverDatagram dtls = new DTLSOverDatagram();
+            InetSocketAddress isa = new InetSocketAddress(host, port);
+
+            SSLEngine engine = doDTLSHandshake(socket, isa, dtls);
+
+            dtls.deliverAppData(engine, socket, ByteBuffer.wrap(filePath), isa);
+            dtls.receiveAppData(engine, socket);
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Unable to do DTLS");
+        }
+    }
+
+    private static SSLEngine doDTLSHandshake(DatagramSocket socket, InetSocketAddress isa, DTLSOverDatagram dtls) throws Exception {
+        SSLEngine engine = dtls.createSSLEngine(true);
+        dtls.handshake(engine, socket, isa, "Client");
+        return engine;
+    }
+
+
+}
--- Client/src/main/java/com/afonsovilalonga/Client.java
+++ Client/src/main/java/com/afonsovilalonga/Client.java
@@ -1,6 +1,7 @@
 package com.afonsovilalonga;
 
 import com.afonsovilalonga.AutomatedClient.AutomatedClient;
+import com.afonsovilalonga.AutomatedClient.Injector;
 import com.afonsovilalonga.InteractiveClient.InteractiveClient;
 import com.afonsovilalonga.Utils.Config;
 
@@ -18,11 +19,25 @@ public class Client {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }else{
+        } else if(args[0].equals("async")){
             for(int i = 0; i < Integer.parseInt(args[1]); i++){
                 Thread t = new Thread(new AutomatedClient());
                 t.start();
             }
+        } else if(args[0].equals("async/notimer")){
+            String protocol = args[1];
+            String file = args[2];
+            int num_threads = Integer.parseInt(args[3]);
+            int num_reqs = Integer.parseInt(args[4]);
+
+            for(int i = 0; i < num_threads; i++){
+                Thread t = new Thread(new Injector(protocol, file, num_reqs));
+                t.start();
+            }
+        }
+        else{
+            System.out.println("Invalid arguments: interactive or async x (where x is the number of threads to spawn) or async/notimer protocol file x (where x is the number of threads to spawn) y (where y is the number of requests per thread)");
+            System.exit(-1);
         }
     }
 
